2b6a93f9a20e3078646032a38eee556646e45b44
eaglesunshine <18900927316@163.com>
2019-05-30 01:32:48 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,5 +1,5 @@
 module.exports = function main() {
-	var res=[];
+	var res1=[],res2=[],res3=[],res=[];
 	
 	if(arguments.length<1)
 		console.log("need input more arguments!");
@@ -10,30 +10,31 @@ module.exports = function main() {
 		var source2=['|.|' ,  '..|'  , '._|' ,  '._|' ,  '|_|'  , '|_.' ,  '|_.' ,  '..|' ,  '|_|'  , '|_|'];
 		var source3=['|_|' ,  '..|'  , '|_.'  , '._|'  , '..|'  , '._|'  , '|_|' ,  '..|'  , '|_|'  , '..|'];
 		
-		var num=[];
-		for(var i=0;i<String(NUM).length;i++)
-		{
-			num[i]=NUM%10;
-			NUM=NUM/10;
-		}
+		var num=[],j=0;
+		while(NUM!=0){
+			num[j]=NUM%10;
+			NUM=Math.floor(NUM/10);
+			j++;
+		} 
 		
 		for(var i=num.length-1;i>=0;i--)
 		{
-			res[0]+=sourse1[i];
-			res[1]+=sourse2[i];
-			res[2]+=sourse3[i];
-			if(i>0 && i<num.length-1)
+			res1+=source1[num[i]];
+			res2+=source2[num[i]];
+			res3+=source3[num[i]];
+			if(i>0)
 			{
-				res[0]+=' ';
-				res[1]+=' ';
-				res[2]+=' ';
+				res1+=' ';
+				res2+=' ';
+				res3+=' ';
 			}
 		}
-		res[0]+='\n';
-		res[1]+='\n';
-		res[2]+='\n';
-		res[0]=res[0]+res[1]+res[2];
+		
+		res1+='\n';
+		res2+='\n';
+		res3+='\n';
+		res=res1+res2+res3;
 	}
 	
-    return res[0];
+    return res;
 };
